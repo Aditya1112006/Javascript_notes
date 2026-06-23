c78d7560972f4dfe96f406ec47a53e345d469ac2
Aditya <adityachauhan1112006@gmail.com>
2026-06-23 16:06:23 +0530

    First commit

---
+++ Operators.js
@@ -0,0 +1,28 @@
+// Arithmetic Operators
+let a = 10;
+let b = 5;
+console.log("Addition: " + (a + b));
+console.log("Subtraction: " + (a - b));
+console.log("Multiplication: " + (a * b))
+console.log("Division: " + (a / b));
+console.log("Modulus: " + (a % b)); 
+console.log("Exponentiation: " + (a ** b));
+
+// Assignment Operators
+a += 5;
+console.log(a);
+a -= 3;
+console.log(a);
+a *= 2;
+console.log(a);
+a /= 4;
+console.log(a);
+a %= 3;
+console.log(a);
+a **= 2;
+console.log(a);
+
+// Unary Operators
+let c = 10;
+console.log(++c);
+console.log(c--);
