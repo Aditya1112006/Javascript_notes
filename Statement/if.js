d3881a2736a1544417c98c823c59d8c30af01488
Aditya <adityachauhan1112006@gmail.com>
2026-06-24 12:43:50 +0530

    learned if statement and comparison operator

---
+++ Statement/if.js
@@ -0,0 +1,16 @@
+// if statement
+
+let age = 23;
+if(age >= 18){
+    console.log("You are eligible to vote.");
+    console.log("You can apply for a driving license.");
+    console.log("You can apply for a passport.");
+    let a = 10;
+    console.log(a*5);
+}
+
+if (age < 18){
+    console.log("You are not eligible to vote.");
+    console.log("You cannot apply for a driving license.");
+    console.log("You cannot apply for a passport.");
+}
