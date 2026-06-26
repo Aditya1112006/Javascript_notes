6e6a0e475b0310524e7fb48b4c403913d88bc045
Aditya <adityachauhan1112006@gmail.com>
2026-06-26 19:29:18 +0530

    Methods commit

---
+++ String_Methods/slice.js
@@ -0,0 +1,10 @@
+let name = "Mugiwara no Rufi";
+console.log(name.slice(12));
+console.log(name.slice(4,9));
+console.log(name.slice(-7));
+
+// Replace
+console.log(name.replace("Rufi","Ichimi"));
+
+// Repeat
+console.log(name.repeat(3));
---
+++ String_Methods/z_practise_q.js
@@ -0,0 +1,11 @@
+let msg = "help!"
+console.log(msg.trim().toUpperCase());
+
+let name = "ApnaCollege";
+console.log(name.slice(4, 9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna","Our"));
+console.log(name.slice(4).replace('l','t'));
+let newstr = name.slice(4).replace('l','t');
+
+console.log(newstr.replace('l','t'));
