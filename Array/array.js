c95a923c991e5d0f7b5d9f4d634a046176d8626a
Aditya <chauhanaditya1112006@gmail.com>
2026-06-27 16:14:54 +0530

    array

---
+++ Array/array.js
@@ -0,0 +1,18 @@
+// Array :- Linear collection of things
+
+let students = ["Aditya","Mayank","Anmol"];
+console.log(students);
+
+
+let num = [4,6,2,7,3];
+console.log(num[3]);
+console.log(typeof num);
+console.log(num.length);
+
+// Arrays are mutable
+
+let marvel = ["Spider man","thor","Captain America"];
+marvel[2] = "Iron man";
+marvel[8] = "hawkeye";
+console.log(marvel);
+
