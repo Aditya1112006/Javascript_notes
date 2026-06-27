9b2e2774af38aae2f9c80cf854fa01cecd0950f8
Aditya <chauhanaditya1112006@gmail.com>
2026-06-27 16:26:37 +0530

    Array methods

---
+++ Array/array_methods.js
@@ -0,0 +1,18 @@
+let bikes = ["Hunter350", "classic350", "Meteor350", "Continental GT 650", "Himalayan350"];
+console.log(bikes);
+
+//push
+bikes.push("Bullet350");
+console.log(bikes);
+
+//pop
+console.log(bikes.pop());
+console.log(bikes);
+
+//unshift
+bikes.unshift("Splendor+");
+console.log(bikes);
+
+//shift
+console.log(bikes.shift());
+console.log(bikes);
