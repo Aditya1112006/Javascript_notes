48b74b1f76917708b60a27cbbcb1c99691a8f2fc
Aditya <chauhanaditya1112006@gmail.com>
2026-06-29 12:19:18 +0530

    Nested for loop

---
+++ Loops/nested_for.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+        console.log(i, j);
+    }
+}
+
+for (let i = 1; i <= 4; i++) {
+    let row = "";
+
+    for (let j = 1; j <= 4; j++) {
+        row += j;
+    }
+    console.log(row);
+}
