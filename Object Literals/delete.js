f21b9fb5c87dd3626eb2f1f85552c711cd9bd64b
Aditya <chauhanaditya1112006@gmail.com>
2026-07-01 13:29:01 +0530

    delete OL

---
+++ Object Literals/delete.js
@@ -0,0 +1,13 @@
+const student = {
+    name : "rahul",
+    rollno : 23,
+    city : "delhi",
+    grade : 92,
+    gender : 'male',
+    hobbies: "Playing"
+}
+
+console.log(student)
+
+delete student.hobbies
+console.log(student)
