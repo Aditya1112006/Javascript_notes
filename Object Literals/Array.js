76b7df05d3c2ce2bf727eecad50d8458d1202d4d
Aditya <chauhanaditya1112006@gmail.com>
2026-07-01 13:43:11 +0530

    array OL

---
+++ Object Literals/Array.js
@@ -0,0 +1,39 @@
+let studentinfo = [
+    {
+        name : "aditya",
+        grade : 'B',
+        place : 'Sirmaur'
+    },
+    {
+        name : "pratham",
+        grade : 'A+',
+        place : 'Hamirpur'
+    },
+    {
+        name : "khushbu",
+        grade : 'A',
+        place : 'Solan'
+    },
+    {
+        name : "nabanita",
+        grade : 'A',
+        place : 'Delhi'
+    },
+    {
+        name : "riya",
+        grade : 'O',
+        place : 'Patiala'
+    }
+]
+
+console.log(studentinfo)
+
+console.log(studentinfo[0])
+console.log(studentinfo[0].name)
+
+
+studentinfo[1].grade = 'C';
+
+console.log(studentinfo[1].grade)
+console.log(studentinfo[1])
+console.log(studentinfo)
