28f18204f6817ca3acfe66f6691b67e2e9c45762
Aditya <chauhanaditya1112006@gmail.com>
2026-07-01 13:26:46 +0530

    add&update OL

---
+++ Object Literals/add&update.js
@@ -0,0 +1,18 @@
+const student = {
+    name : "rahul",
+    rollno : 23,
+    city : "delhi",
+    grade : 92
+}
+
+console.log(student);
+
+student.city = 'mumbai'
+console.log(student);
+
+student.gender = "male"  // create and add to the object
+console.log(student);
+
+student.grade = 'A'
+
+console.log(student);
