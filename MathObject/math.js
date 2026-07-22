c64b553ec8610d82c1da7483b4a28b09a871c15e
Aditya <chauhanaditya1112006@gmail.com>
2026-07-22 21:46:54 +0530

    Math object operations

---
+++ MathObject/math.js
@@ -0,0 +1,17 @@
+// console.log(Object.getOwnPropertyNames(Math));
+
+console.log(Math.PI)
+
+console.log(Math.E)
+
+console.log(Math.abs(-12))
+
+console.log(Math.pow(2,4))
+
+console.log(Math.ceil(7.45))
+
+console.log(Math.floor(7.45))
+
+console.log(Math.random())
+console.log(Math.random())
+console.log(Math.random())
