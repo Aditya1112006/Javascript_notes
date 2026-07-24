dc7450f9355ca5cb824d2e790b13aec54a974392
Aditya <chauhanaditya1112006@gmail.com>
2026-07-24 22:56:18 +0530

    poem with function

---
+++ Functions/poem .js
@@ -0,0 +1,7 @@
+function printPoem(){
+    console.log("If you can dream-and not make dreams your master")
+    console.log("If you can think-and not make thoughts your aim")
+    console.log("If you can meet with Triumph and Disaster And treat those two impostors just the same")
+}
+
+printPoem();
