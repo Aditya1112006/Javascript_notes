91d70cb1b83bb4566a360be76b0d22241f3c9759
Aditya <chauhanaditya1112006@gmail.com>
2026-07-26 22:28:45 +0530

    block scope

---
+++ Scope/block scope/block.js
@@ -0,0 +1,13 @@
+// Note :- block scope can only be apply on const and let keyword
+
+// {
+//     let a = 25;
+// }
+
+// console.log(a);
+
+for(let i = 1;i<=5;i++){
+    console.log(i);
+}
+
+// console.log(i);
