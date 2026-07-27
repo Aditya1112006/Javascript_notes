7bfbbb5fe266f3f1e792c9d889079fb061ba89f8
Aditya <chauhanaditya1112006@gmail.com>
2026-07-27 21:09:30 +0530

    High order functions 2

---
+++ Functions/High Order function/HOF2.js
@@ -0,0 +1,18 @@
+function OddEven(request){
+    if(request == "odd"){
+        return function(n){
+            console.log(!(n%2==0));
+        }
+    }else if (request == "even"){
+        return function(n){
+            console.log(n%2==0);
+        }
+    }else{
+        console.log("wrong request");
+    }
+}
+
+let request = "odd";
+
+let func = OddEven(request);
+func(3);
