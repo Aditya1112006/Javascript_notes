c00428886673e3088ea0ef2d1566496ac2199cf4
Aditya <chauhanaditya1112006@gmail.com>
2026-07-27 20:59:07 +0530

    High order functions

--- Functions/High Order function/HOF.js
+++ Functions/High Order function/HOF.js
@@ -1,4 +1,4 @@
-// HOF :-  High order function
+// HOF :-  High order function - Taking Functions as a argument in another function
 
 function MultipleFunction(func, n){
     for(let i = 1; i<=n; i++){
