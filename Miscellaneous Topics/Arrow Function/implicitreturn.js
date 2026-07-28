944762d3e27c943f8856abb2993c8197f1762337
Aditya <chauhanaditya1112006@gmail.com>
2026-07-28 22:40:43 +0530

    Implicit return in arrow function

---
+++ Miscellaneous Topics/Arrow Function/implicitreturn.js
@@ -0,0 +1,15 @@
+// Implicit return in arrow function
+
+const pow = (a , b) =>
+    a**b;;
+
+console.log(pow(2,4));
+
+
+// We can also use paranthesis
+
+const cube = n =>(
+    n*n*n
+);
+
+console.log(cube(3));
