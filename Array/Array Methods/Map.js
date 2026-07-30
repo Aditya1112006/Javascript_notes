792a65ae1df3b6daa496a553762a5df7905f79ac
Aditya <chauhanaditya1112006@gmail.com>
2026-07-30 21:14:25 +0530

    Map method

---
+++ Array/Array Methods/Map.js
@@ -0,0 +1,11 @@
+let Cars = [
+  "Bugatti La Voiture Noire",
+  "Rolls-Royce Boat Tail",
+  "Pagani Zonda HP Barchetta",
+  "Bugatti Centodieci",
+  "Lamborghini Veneno"
+];
+
+let limitedEdition = Cars.map((each)=> {
+    console.log(each);
+})
