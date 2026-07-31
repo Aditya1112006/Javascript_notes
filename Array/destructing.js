e3c15d31ecb903bc62f0da9676ea1647a7563f56
Aditya <chauhanaditya1112006@gmail.com>
2026-07-31 21:42:48 +0530

    Destructuring

---
+++ Array/destructing.js
@@ -0,0 +1,18 @@
+let names = ["tony", "bruce", "steve", "peter"];
+let [winner, runnerup] = names;
+console.log(winner, runnerup);
+
+// Destructing (objects)
+
+const student = {
+    name: "karan",
+    class: 9,
+    age: 14,
+    subjects: ["hindi", "english", "math", "science", "social studies"],
+    username:
+    "karan123",
+    password:
+    1234,
+};
+const { username: user, password: pass } = student;
+console.log(user); //"karan123"
