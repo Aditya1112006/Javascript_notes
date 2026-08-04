9e21f1fd7a9ddc34bc1468f27f62da16f83a44d9
Aditya <chauhanaditya1112006@gmail.com>
2026-08-04 22:02:24 +0530

    app.js modified

---
+++ DOM/app.js
@@ -0,0 +1,6 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i = 0;i<smallImages.length;i++){
+//     console.dir(smallImages[i].src = "Spider man.jpg");
+//     console.log(`Value of image no. ${i} is changed`);
+// };
