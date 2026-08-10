e9a4db921e85dfda2d2d91addaf0166047fba134
Aditya <chauhanaditya1112006@gmail.com>
2026-08-10 22:46:40 +0530

    Solved some questons

--- DOM/app.js
+++ DOM/app.js
@@ -1,6 +1,27 @@
-// let smallImages = document.getElementsByClassName("oldImg");
+// Inner text gives us the content that is shown on the live server screen.
+// Inner HTML gives full html markup
+// Text content gives us the content which we have written inside the html file.
 
-// for(let i = 0;i<smallImages.length;i++){
-//     console.dir(smallImages[i].src = "Spider man.jpg");
-//     console.log(`Value of image no. ${i} is changed`);
-// };
+let para1 = document.createElement("p");
+para1.innerText = "Hey, i am Spidey";
+
+document.querySelector("body").append(para1);
+para1.classList.add("red");
+
+let head1 = document.createElement("h3");
+head1.innerText = "Hey, I am captain";
+document.querySelector('body').append(head1);
+
+head1.classList.add('blue');
+
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para2 = document.createElement("p");
+
+h1.innerText = "Hey, what's up";
+para2.innerText = "Me too!";
+
+div.append(h1);
+div.append(para2);
+div.classList.add('box');
+document.querySelector('body').append(div);
