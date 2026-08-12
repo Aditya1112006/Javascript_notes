ffd224d865086e7ccc628ee9379eb74db94a08b2
Aditya <chauhanaditya1112006@gmail.com>
2026-08-12 22:18:59 +0530

    keyboard event

--- DOM/Part3/app.js
+++ DOM/Part3/app.js
@@ -1,11 +1,19 @@
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function(e) {
-    console.log(e);
-    console.log("Single click");
-});
+let inp = document.querySelector("input");
 
-btn.addEventListener("dblclick", function(e) {
-    console.log(e);
-    console.log("Double click");
+inp.addEventListener("keydown", function(e){
+    console.log("code = ", e.code);
+    if(e.code == "ArrowUp"){
+        console.log("Character moves forward");
+    }
+    else if(e.code == "ArrowDown"){
+        console.log("Character moves backward");
+    }
+    else if(e.code == "ArrowLeft"){
+        console.log("Character moves left");
+    }
+    else if(e.code == "ArrowRight"){
+        console.log("Character moves right");
+    }
 });
