647e3ff564ec17f97b8f7f90b74e302b1b3e9f20
Aditya <chauhanaditya1112006@gmail.com>
2026-08-20 22:20:46 +0530

    Promise

--- Promises/app.js
+++ Promises/app.js
@@ -2,9 +2,17 @@ function saveData(data){
     return new Promise((resolve, reject) =>{
         let internetspeed = Math.floor(Math.random()*10) + 1;
         if(internetspeed > 4){
-            resolve("Data saved");
+            resolve("sucess : Data saved");
         }else{
-            reject("Data not saved, Internet interrupted");
+            reject("Failure : Data not saved, Internet interrupted");
         }
     })
 };
+
+saveData("Mugiwara")
+    .then(()=>{
+        console.log("Promise was resolved");
+    })
+    .catch(()=>{
+        console.log("Promise was rejected");
+    })
