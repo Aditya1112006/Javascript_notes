58815fcc424e608ea305aba5a6b002c86f296d17
Aditya <chauhanaditya1112006@gmail.com>
2026-08-21 22:06:20 +0530

    Error handling

--- Await&async/app.js
+++ Await&async/app.js
@@ -3,6 +3,10 @@ let h1 = document.querySelector("h1");
 function colorChange(color, delay){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
+            let num = Math.floor(Math.random()*5)+1;
+            if(num>3){
+                reject("Error caught");
+            }
             h1.style.color = color;
             console.log("color changed to ", color);
             resolve("Color changed");
@@ -11,10 +15,17 @@ function colorChange(color, delay){
 }
 
 async function demo() {
-    await colorChange("red",1000);
-    await colorChange("blue",1000);
-    await colorChange("orange",1000);
-    await colorChange("green",1000);
+    try{
+        await colorChange("red",1000);
+        await colorChange("blue",1000);
+        await colorChange("orange",1000);
+        await colorChange("green",1000);
+    }catch(err){
+        console.log(err);
+    }
+
+    let a = 5;
+    console.log(a);
 }
 
 demo()
