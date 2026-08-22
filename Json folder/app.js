2b1a9d3fa51fff54cb167252947c09efc9a2b013
Aditya <chauhanaditya1112006@gmail.com>
2026-08-22 22:15:22 +0530

    JSON stringify

--- Json folder/app.js
+++ Json folder/app.js
@@ -4,3 +4,11 @@ let validres = JSON.parse(val);
 
 console.log(validres);
 console.log(validres.fact);
+
+let student = {
+    name : "Aditya",
+    rollno : 21
+}
+
+let json = JSON.stringify(student);
+console.log(json);
