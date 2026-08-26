653a5e71f126331743a8febad885207bae82f8ac
Aditya <chauhanaditya1112006@gmail.com>
2026-08-26 20:05:30 +0530

    Header in axios

---
+++ Axios/Headers with axios/app.js
@@ -0,0 +1,13 @@
+url = "https://icanhazdadjoke.com/";
+
+async function getjokes() {
+    try{
+        const config = {headers : {Accept : "application/json"}};
+        let res = await axios.get(url,config);
+        console.log(res.data);
+    }
+    catch(err){
+        console.log(err);
+        return "No match";
+    }
+}
